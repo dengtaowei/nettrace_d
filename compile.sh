6294fecbbfee2ed0f97e7b8d703d0e50cf9881e1
root <1158725668@qq.com>
2025-08-09 15:47:27 +0800

    add bpf debug info for vscode debug

--- compile.sh
+++ compile.sh
@@ -18,6 +18,9 @@ make NO_GLOBAL_DATA=1 ARCH=x86 install
 # with my own headers from btf
 make NO_BTF=1 NO_GLOBAL_DATA=1 BPF_MAKE_HEADERS=1 KERNEL=/home/anlan/Desktop/nettrace_d/src/progs/kheaders/x86 ARCH=x86 install
 
+# with kernel debug
+make NO_BTF=1 NO_GLOBAL_DATA=1 BPF_MAKE_HEADERS=1 KERNEL=/home/anlan/Desktop/nettrace_d/src/progs/kheaders/x86 ARCH=x86 BPF_DEBUG=1 install
+
 
 # arm64
 make 3rdparty/clean
@@ -49,3 +52,8 @@ make check_feat=0 EXTRA_LDFLAGS="-L../../../3rdparty/install/x86/lib --static"
 make check_feat=0 CROSS_COMPILE=aarch64-linux-gnu- CC=aarch64-linux-gnu-gcc EXTRA_LDFLAGS="-L../../../3rdparty/install/arm64/lib --static"
 
 make check_feat=0 CROSS_COMPILE=arm-linux-gnueabihf- CC=arm-linux-gnueabihf-gcc EXTRA_LDFLAGS="-L../../../3rdparty/install/arm/lib --static"
+
+# display bpf_printk message
+sudo bpftool prog tracelog
+
+
