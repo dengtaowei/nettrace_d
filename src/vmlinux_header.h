f990e8bb32874450886fd28aa292242d56dc8a8a
dengtaowei <1158725668@qq.com>
2025-08-12 09:32:59 +0800

    add kstruct for arm

--- src/vmlinux_header.h
+++ src/vmlinux_header.h
@@ -34,6 +34,8 @@
 #define NT_DISABLE_NFT 1
 #if defined(__TARGET_ARCH_x86)
 #include "./progs/kheaders/x86/kheaders_x86.h"
+#elif defined(__TARGET_ARCH_arm)
+#include "./progs/kheaders/arm/kheaders_arm.h"
 #elif defined(__TARGET_ARCH_arm64)
 #include "./progs/vmlinux_arm64.h"
 #elif defined(__TARGET_ARCH_loongarch)
